9a7ea4fe81885a61aa2209243de8c31c91247efa
azibali01 <azibaliansari311@gmail.com>
2025-11-29 20:14:53 +0500

    Add VITE_API_PROD_URL type definition to environment

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,13 @@
 interface ImportMetaEnv {
-    readonly VITE_API_BASE_URL?: string
-    readonly VITE_API_WITH_CREDENTIALS?: string
-    readonly NODE_ENV?: string
-    readonly MODE?: string
-    readonly PROD?: boolean
-    readonly DEV?: boolean
+  readonly VITE_API_BASE_URL?: string;
+  readonly VITE_API_PROD_URL?: string;
+  readonly VITE_API_WITH_CREDENTIALS?: string;
+  readonly NODE_ENV?: string;
+  readonly MODE?: string;
+  readonly PROD?: boolean;
+  readonly DEV?: boolean;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
